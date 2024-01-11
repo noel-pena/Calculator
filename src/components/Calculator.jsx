6e4b1eb245f0ee028f00b689255af4a3d801732e
noel-pena <azulu88@hotmail.com>
2024-01-11 18:42:32 -0500

    more commit

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,44 +3,70 @@ import * as React from "react";
 import Button from '@mui/material/Button';
 import { redButton, grayButton, orangeButton, yellowButton } from "./Styles";
 import BackspaceIcon from '@mui/icons-material/Backspace';
+import { useState } from "react";
 
 
 export const Calculator = () => {
+
+    const [input, setInput] = useState('')
+
+
+    function handleInput(event){
+        setInput(input.slice(0, 12) + event.target.value);
+    }
+
+    function handleClear(){
+        setInput('')
+    }
+
+    function handleDelete(){
+        setInput(input.slice(0, -1));
+    }
+
+    function handleEquals(){
+        setInput(eval(input));
+    }
+
     return (
         <Grid className="calculator">
-            <Grid className="entry" xs={12} m={0} p={2} textAlign="end">
-                123456
+            <Grid className="entry" xs={12} m={0} p={2} >
+                <input 
+                    type="text" 
+                    value={input} 
+                    textAlign="end"
+                    maxLength="10"
+                />
             </Grid>
             <Grid sx={{ display: "flex", flexDirection: "row"}} px={1}>
                 <Grid spacing={2} xs={4}>
-                    <Button style={redButton}>C</Button>
-                    <Button style={grayButton}>7</Button>
-                    <Button style={grayButton}>4</Button>
-                    <Button style={grayButton}>1</Button>
-                    <Button style={grayButton}>0</Button>
-                </Grid>
-                <Grid spacing={2} xs={4}>
-                    <Button style={orangeButton}>+/-</Button>
-                    <Button style={grayButton}>8</Button>
-                    <Button style={grayButton}>5</Button>
-                    <Button style={grayButton}>2</Button>
-                    <Button style={yellowButton}>
-                        {<SvgIcon component={BackspaceIcon} />}
-                    </Button>
-                </Grid>
-                <Grid spacing={2} xs={4}>
-                    <Button style={orangeButton}>%</Button>
-                    <Button style={grayButton}>9</Button>
-                    <Button style={grayButton}>6</Button>
-                    <Button style={grayButton}>3</Button>
-                    <Button style={yellowButton}>.</Button>
-                </Grid>
-                <Grid spacing={2} xs={4}>
-                    <Button style={yellowButton}>/</Button>
-                    <Button style={yellowButton}>*</Button>
-                    <Button style={yellowButton}>+</Button>
-                    <Button style={yellowButton}>-</Button>
-                    <Button style={yellowButton}>=</Button>
+                        <Button onClick={handleClear} style={redButton} >C</Button>
+                        <Button onClick={handleInput} value="7" style={grayButton}>7</Button>
+                        <Button onClick={handleInput} value="4" style={grayButton}>4</Button>
+                        <Button onClick={handleInput} value="1" style={grayButton}>1</Button>
+                        <Button onClick={handleInput} value="0" style={grayButton}>0</Button>
+                    </Grid>
+                    <Grid spacing={2} xs={4}>
+                        <Button style={orangeButton}>+/-</Button>
+                        <Button onClick={handleInput} value="8" style={grayButton}>8</Button>
+                        <Button onClick={handleInput} value="5" style={grayButton}>5</Button>
+                        <Button onClick={handleInput} value="2" style={grayButton}>2</Button>
+                        <Button onClick={handleDelete} style={yellowButton}>
+                            {<SvgIcon component={BackspaceIcon} />}
+                        </Button>
+                    </Grid>
+                    <Grid spacing={2} xs={4}>
+                        <Button value="%" style={orangeButton}>%</Button>
+                        <Button onClick={handleInput} value="9" style={grayButton}>9</Button>
+                        <Button onClick={handleInput} value="6" style={grayButton}>6</Button>
+                        <Button onClick={handleInput} value="3" style={grayButton}>3</Button>
+                        <Button onClick={handleInput} value="." style={yellowButton}>.</Button>
+                    </Grid>
+                    <Grid spacing={2} xs={4}>
+                        <Button onClick={handleInput} value="/" style={yellowButton}>/</Button>
+                        <Button onClick={handleInput} value="*" style={yellowButton}>*</Button>
+                        <Button onClick={handleInput} value="+" style={yellowButton}>+</Button>
+                        <Button onClick={handleInput} value="-" style={yellowButton}>-</Button>
+                        <Button onClick={handleEquals} style={yellowButton}>=</Button>
                 </Grid>
                 <Grid xs={6}>
 
