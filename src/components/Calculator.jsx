876c8fceb455e71374544fe2022eaa01b011b074
noel-pena <azulu88@hotmail.com>
2024-01-12 15:23:42 -0500

    more components

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -10,8 +10,32 @@ export const Calculator = () => {
 
     const [input, setInput] = useState('')
 
-
     function handleInput(event){
+
+        // Attempting to add key responses
+        // const key = event.target.value
+
+        // switch (key) {
+        //     case 'clear':
+        //         handleClear()
+        //         break;
+        //     case 'handleDelete':
+        //         handleDelete()
+        //         break;
+        //     case '+':
+        //     case '-':
+        //     case '*':
+        //     case '/':
+        //         setInput(input.concat(key));
+        //         break;
+        //     case '=':
+        //         handleEquals();
+        //         break;
+        //     default:
+        //         setInput(input.concat(key));
+        //         break;
+        // }
+
         setInput(input.slice(0, 12) + event.target.value);
     }
 
@@ -23,8 +47,23 @@ export const Calculator = () => {
         setInput(input.slice(0, -1));
     }
 
+    function handlePercent(){
+        const currentValue = parseFloat(input);
+        const percentValue = currentValue / 100;
+        setInput(percentValue.toString())
+    }
+
+    function handleNegative(){
+        const currentValue = parseFloat(input);
+        currentValue < 0 ? setInput(Math.abs(currentValue).toString()) : setInput((-1 * currentValue).toString())
+    }
+
     function handleEquals(){
-        setInput(eval(input));
+        try{
+            setInput(eval(input.slice(0, 12)));
+        } catch (error) {
+            setInput("Error");
+        }
     }
 
     return (
@@ -46,16 +85,16 @@ export const Calculator = () => {
                         <Button onClick={handleInput} value="0" style={grayButton}>0</Button>
                     </Grid>
                     <Grid spacing={2} xs={4}>
-                        <Button style={orangeButton}>+/-</Button>
+                        <Button onClick={handleNegative} style={orangeButton}>+/-</Button>
                         <Button onClick={handleInput} value="8" style={grayButton}>8</Button>
                         <Button onClick={handleInput} value="5" style={grayButton}>5</Button>
                         <Button onClick={handleInput} value="2" style={grayButton}>2</Button>
-                        <Button onClick={handleDelete} style={yellowButton}>
+                        <Button onClick={handleDelete} id="handleDelete" style={yellowButton}>
                             {<SvgIcon component={BackspaceIcon} />}
                         </Button>
                     </Grid>
                     <Grid spacing={2} xs={4}>
-                        <Button value="%" style={orangeButton}>%</Button>
+                        <Button onClick={handlePercent} value="%" style={orangeButton}>%</Button>
                         <Button onClick={handleInput} value="9" style={grayButton}>9</Button>
                         <Button onClick={handleInput} value="6" style={grayButton}>6</Button>
                         <Button onClick={handleInput} value="3" style={grayButton}>3</Button>
